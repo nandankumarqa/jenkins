f4604b7997b6f3543efc0d87432b2e1c9a3d730f
nandankumar@csgroupchd.com <nandankumar@csgroupchd.com>
2022-03-14 13:34:11 +0530

    test

--- src/test/java/Resources/ExtentReportListener1.java
+++ src/test/java/Resources/ExtentReportListener1.java
@@ -18,13 +18,12 @@ import com.relevantcodes.extentreports.ExtentReports;
 import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
 
-public class ExtentReportListener implements IReporter {
+public class ExtentReportListener1 implements IReporter {
 
 	private ExtentReports extent;
 
 	public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
-		String dir = System.getProperty("user.dir");
-		extent = new ExtentReports(dir+"//test-output//report//Extent.html", true);
+		extent = new ExtentReports(outputDirectory + File.separator + "./report/Extent.html", true);
 
 		for (ISuite suite : suites) {
 			Map<String, ISuiteResult> result = suite.getResults();
